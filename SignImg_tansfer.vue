2373e2ae5abcf9ca7f13c4a9c15e0659dd25bc41
Olive-2019 <1497250390@qq.com>
2023-12-07 20:26:15 +0800

    整理代码

---
+++ SignImg_tansfer.vue
@@ -0,0 +1,80 @@
+<template>
+  
+  <section v-if="t_preview" addinName="name" style="{'width': colWidth}">
+      <span v-if="args.required || args.label" style="{'width': labelWidth, 'display': 'inline-block',
+      'padding-left':'16px','text-align': labelXAlign, 'vertical-align': labelYAlign,
+      'background-color': args.label_color, 'padding-right': '10px'}">
+        <span v-if="args.required" style="color: red">*</span>
+        <label class="msize" style="{'color': args.label_fontColor, 'font-size': args_lfsize}">{{ args.label }}</label>
+      </span>
+      <span style="{'height': arg_height, 'line-height': arg_height, 'width': mainWidth, 'display': 'inline-block','background-color': args.main_color, 'text-align': mainXAlign, 'vertical-align': mainYAlign}">
+          <van-button icon="edit" type="default">{{ args.signText }}</van-button>
+      </span>
+
+      
+      <slot name="widget-footer"></slot>
+      <span v-show="t_edit" ref="edit">
+          <EditBox v-if="actEdit" addinName="name" widgetAnnotation="widgetAnnotation" editExtendInfo="editExtendInfo" ref="editbox" v-model="args" attributes="filter_attributes" router="router" route="route" root="root" itemValue="itemValue" dataTypes="dataTypes" targetclass="itemValue.data.targetClass">
+              <div slot="attribute">
+                <p class="margin1" id="pSignB955068">
+{{$t(SignImg.pSignB955068.innerContent)}}
+</p>
+                <Input class="margin1" v-model="args.signText" maxlength="8" show-word-limit=show-word-limit id="InputSignB492074"/>
+                <Row type="flex" align="middle">
+                  <Col span="6">
+                    <span id="lineWidthText">
+{{$t(SignImg.lineWidthText.innerContent)}}
+</span>
+                  </Col>
+                  <Col span="18">
+                    <InputNumber id="lineWidthValue" max="10" min="1" v-model="args.lineWidth"></InputNumber>
+                  </Col>
+                </Row>
+                <div class="margin1" id="divSignB025128">
+                    <p class="margin1" style="float: left; margin-right: 10px;" id="pSignB573576">
+{{$t(SignImg.pSignB573576.innerContent)}}
+</p>
+                    <ColorPicker v-model="args.lineColor" id="ColorPickerSignB911114"/>
+                </div>
+                <div class="margin1" id="divSignB025129">
+                    <p class="margin1" style="float: left; margin-right: 10px;" id="pSignB573577">
+{{$t(SignImg.pSignB573577.innerContent)}}
+</p>
+                    <ColorPicker v-model="args.bgColor" id="ColorPickerSignB911115"/>
+                </div>
+                <div class="margin1">
+                  存储类型
+                  <RadioGroup class="margin1" v-model="args.saveType" style="margin-left: 10px;">
+                    <Radio label="base">Base64</Radio>
+                    <Radio label="file">文件</Radio>
+                  </RadioGroup>
+                </div>
+                <div class="margin1" id="divSignB218599">
+{{$t(SignImg.divSignB218599.innerContent)}}
+<i-switch style="float: right" v-model="args.isCrop" id="i-switchSignB746719"/>
+                </div>
+              </div>
+              <div slot="layout">
+                <div class="margin1" style="margin: 10px 0 10px 0" id="divEDB599997">
+{{$t(SignImg.divEDB599997.innerContent)}}
+<i-switch style="float: right" v-model="args.horFlag" disabled="args.readonlyDisabled" id="i-switchEDB709199"/>
+                </div>
+              </div>
+              <div slot="operation">
+                  
+              </div>
+          </EditBox>
+      </span>
+  </section>
+  <section v-else=v-else style="text-align: center" addinName="name">
+      <span style="text-align:center">
+          <div class="form-addin-icon">
+              <i class="iconfont">&#xe726;</i>
+          </div>
+
+          <div class="form-addin-name">
+          签名
+          </div>
+      </span>
+  </section>
+</template>
--- transfer.js
+++ transfer.js
@@ -1,19 +1,21 @@
 const { parse, stringify } = require( 'vue-i18n-translation-parser');
 const fs = require('fs');
+// 转换后的键值对输出
 let outputjs = {}
+// 还需要加id的内容，目前写入content
 let needID = []
 // 读取 JavaScript 文件内容
-const fileName = 'FILE'
+const fileName = 'SignImg'
 const filePath = 'input.vue'; // 替换为你的 JavaScript 文件路径
 const code = fs.readFileSync(filePath, 'utf8');
-// const { ast } = compile(code);
 
-
+// 检测中文
 function hasChinese(content) {
     let chineseRegex = /[\u4e00-\u9fff]/;
       if (chineseRegex.test(content)) return true;
     return false;
 }
+// AST转换
 function exchangeToArg(content, id, type) {
     content = content.replace(/[\n\t\r]/g, '').trim();
     if (id === null || id === undefined) needID.push(content.replace(/[\"]/g, ''));
@@ -24,12 +26,13 @@ function exchangeToArg(content, id, type) {
     }
     return null;
 }
+// 遍历AST
 function i18n(AST, id) {
     if (AST.type === 'text') {
         if (hasChinese(AST.content)) {
             let path = exchangeToArg(AST.content, id, 'innerContent')
             if (path === null) return
-            let value = '{{' + path + '}}';
+            let value = '\n{{' + path + '}}\n';
             AST.content = value;
         }
         return 
@@ -49,33 +52,56 @@ function i18n(AST, id) {
     }
     AST.children.forEach(child => i18n(child, id))
 }
-
-const AST = parse(code);
-i18n(AST[0]);
-console.log(stringify(AST));
-const vueContent = stringify(AST);
-fs.writeFile('output.vue', vueContent, 'utf8', (err) => {
-  if (err) {
-    console.error('Error writing file:', err);
-    return;
-  }
-  console.log('Vue content has been written to output.vue');
-});
-const jsonContent = JSON.stringify(outputjs);
-const needIDContent = JSON.stringify(needID);
-
-fs.writeFile('output.json', jsonContent, 'utf8', (err) => {
-  if (err) {
-    console.error('Error writing file:', err);
-    return;
-  }
-  console.log('JSON data has been written to output.json');
-});
-fs.writeFile('needIDContent.json', needIDContent, 'utf8', (err) => {
+// 写出数据
+function outputFile(data, filename) {
+  fs.writeFile(filename, data, 'utf8', (err) => {
     if (err) {
       console.error('Error writing file:', err);
       return;
     }
     console.log('JSON data has been written to output.json');
   });
-  
+}
+
+// 输出文件的格式化
+function formatStringToJSON(inputString) {
+  const result = {};
+  inputString = inputString.replace(/[\"{]/g, '');
+  // 将输入的字符串按照逗号分隔成键值对
+  const keyValuePairs = inputString.split(',');
+
+  keyValuePairs.forEach(pair => {
+    const [key, value] = pair.split(':').map(item => item.trim());
+    // 将键（key）按照点号拆分成层级结构
+    const keys = key.split('.');
+    let current = result;
+
+    keys.forEach((key, index) => {
+      if (index === keys.length - 1) {
+        // 如果是最后一个键，直接赋予值
+        current[key] = value.replace(/[\}\"]/g, '');
+      } else {
+        // 如果不是最后一个键，创建子对象
+        current[key] = current[key] || {};
+        current = current[key];
+      }
+    });
+  });
+
+  return result;
+}
+
+// 读入AST
+const AST = parse(code);
+// i18n转换
+i18n(AST[0]);
+//写出文件
+const vueContent = stringify(AST);
+const kvContent = JSON.stringify(outputjs);
+const needIDContent = JSON.stringify(needID);
+const formattedJSON = formatStringToJSON(kvContent);
+const jsonContent = JSON.stringify(formattedJSON);
+outputFile(vueContent, fileName + '_tansfer.vue');
+outputFile(needIDContent, fileName + '_stillNeedIDContent.json');
+outputFile(jsonContent, fileName + '_kv.json');
+
